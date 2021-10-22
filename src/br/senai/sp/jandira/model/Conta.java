3844cfedf3f4509ce3ecb08b2f0ee075f7d78610
LEONARDO DE NOVAES VIVI <21276367@CFP127ALUNO.LOCAL>
2021-10-22 16:52:12 -0300

    1.1

--- src/br/senai/sp/jandira/model/Conta.java
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -16,15 +16,15 @@ public class Conta {
 	private String estado;
 
 	public void Estado() {
-		if (resultado < 18.5) {
+		if (resultado <= 18.5) {
 			estado = "Abaixo do Peso";
-		} else if (resultado > 18.5 && resultado < 24.9) {
+		} else if (resultado >= 18.5 && resultado <= 24.9) {
 			estado = "Peso Normal";
-		} else if (resultado > 25 && resultado < 29.9) {
+		} else if (resultado >= 25 && resultado <= 29.9) {
 			estado = "Sobrepeso";
-		} else if (resultado > 30 && resultado < 34.9) {
+		} else if (resultado >= 30 && resultado <= 34.9) {
 			estado = "Obesidade 1";
-		} else if (resultado > 35 && resultado < 39.9) {
+		} else if (resultado >= 35 && resultado <= 39.9) {
 			estado = "Obesidade 2";
 		} else {
 			estado = "Obesidade Mórbida";
